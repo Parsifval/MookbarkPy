347f5a95eb8cfa822b857a25fa2f5cd1596a7ad5
Your Name <you@example.com>
2021-08-04 13:07:40 +0100

    Added bookmark viewing, and opening

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import os
+import pandas as pd
 
 def main():
 
-    open_bookmark('hello')
+    os.system('clear') 
+    print() 
+    read_bookmark() 
+    print() 
+    print("Type 'add' to add a new bookmark")
+    user_choice = input('What would you like to do?: ')
 
-    add_bookmark()
-    read_bookmark()
+    if 'add' in user_choice:
+        add_bookmark()
+
+    else:
+        print()
+        user_choice = int(user_choice)
+        open_bookmark(user_choice)
+
+def read_bookmark():
+
+    df = pd.read_csv('bookmarks.csv')
+    print(df.head())
 
 def add_bookmark(): 
 
     bookmark_url = input('URL of Bookmark: ')
     bookmark_name = input('Name of Bookmark: ')
-    bookmark_tags = input('Tags of Bookmark: ')
+    bookmark_tags = input('Tags of Bookmark: ') #Need to remove commas if present
 
     bookmark_string = f'{bookmark_url}, {bookmark_name}, {bookmark_tags} \n'
 
@@ -19,19 +35,21 @@ def add_bookmark():
     bookmarks.write(bookmark_string)
     bookmarks.close()
 
-def read_bookmark():
+    main()
 
-    bookmarks = open('bookmarks.csv', 'r')
-    a = bookmarks.read()
-    print(a)
+def open_bookmark(num):
 
-def open_bookmark(url):
+    df = pd.read_csv('bookmarks.csv')
+    url = df.loc[num, 'URL']
 
-    os.system('xdg-open https:brave.com')
+    os.system(f'xdg-open {url}')
 
-    
-    
-    
+    main()
+
+def filter_bookmarks(tag):
+    print('Hello')
+
+
 main()
     
 
