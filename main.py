b6b1347b2ad9da90cefe94a5bbdf17e0af4cc39e
Your Name <you@example.com>
2021-08-06 15:17:40 +0100

    updates

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
 import pandas as pd
 
+os.system('clear')
+
 def main():
 
-    os.system('clear') 
     print() 
     read_bookmark() 
     print() 
     print("Type 'add' to add a new bookmark")
+    print("Type 'filter' filter bookmarks by tag")
     user_choice = input('What would you like to do?: ')
 
     if 'add' in user_choice:
         add_bookmark()
+
+    elif 'filter' in user_choice:
+        filter_bookmarks()
 
     else:
         print()
@@ -46,8 +51,25 @@ def open_bookmark(num):
 
     main()
 
-def filter_bookmarks(tag):
-    print('Hello')
+def filter_bookmarks():
+
+
+    df = pd.read_csv('bookmarks.csv')
+
+    search = input('What tag would you like to filter by?: ')
+    print(search)
+
+    a = 0
+    while a < 2:
+        tags = df.iloc[a, 2]
+        print(tags)
+
+        if search in tags:
+            z = df.iloc[2]
+            print(z)
+
+        a += 1
+    main()
 
 
 main()
