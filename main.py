7c958e393ff49a5afcdac5e5e1fd7e3aa9ac4bb4
Your Name <you@example.com>
2021-08-07 19:50:59 +0100

    Added alphabetization

--- main.py
+++ main.py
@@ -3,39 +3,52 @@ import pandas as pd
 
 os.system('clear')
 df = pd.read_csv('bookmarks.csv')
+print(df)
 
 def main():
 
     print() 
-    read_bookmark() 
-    print() 
-    print("Type 'add' to add a new bookmark")
-    print("Type 'filter' filter bookmarks by tag")
+    print('Type 'add' to add a new bookmark')
+    print('Type 'filter' filter bookmarks by tag')
     user_choice = input('What would you like to do?: ')
 
-    if 'add' in user_choice:
-        add_bookmark()
+    try:
+        if 'add' in user_choice:
+            add_bookmark()
 
-    elif 'filter' in user_choice:
-        user_choice = user_choice.replace('filter', '')
-        user_choice = user_choice.replace(' ', '')
-        print(user_choice)
-        filter_bookmarks(user_choice)
+        elif 'filter' in user_choice:
+            user_choice = user_choice.replace('filter', '')
+            user_choice = user_choice.replace(' ', '')
+            filter_bookmarks(user_choice)
 
-    else:
-        print()
-        user_choice = int(user_choice)
-        open_bookmark(user_choice)
+        elif 'abc' in user_choice:
+            alphabetize_bookmarks()
 
-def read_bookmark():
+        elif 'show' in user_choice:
+            print_bookmarks()
 
+        else:
+            print()
+            user_choice = int(user_choice)
+            open_bookmark(user_choice)
+
+    except ValueError:
+        print('Error: Please enter a valid input: ')
+        main()
+
+
+
+def print_bookmarks():
+    
     print(df)
+    main()
 
 def add_bookmark(): 
 
     bookmark_url = input('URL of Bookmark: ')
     bookmark_name = input('Name of Bookmark: ')
-    bookmark_tags = input('Tags of Bookmark: ') #Need to remove commas if present, and lowercase all letters
+    bookmark_tags = input('Tags of Bookmark: ')
+    bookmark_tags = bookmark_tags.replace(',', ' ')
 
     bookmark_string = f'{bookmark_url}, {bookmark_name}, {bookmark_tags} \n'
 
@@ -43,13 +56,19 @@ def add_bookmark():
     bookmarks.write(bookmark_string)
     bookmarks.close()
 
+    print_bookmarks()
     main()
 
 def open_bookmark(bookmark):
 
     url = df.loc[bookmark, 'URL']
-
     os.system(f'xdg-open {url}')
+    main()
+
+def alphabetize_bookmarks():
+    
+    print()
+    print(df.sort_values('URL'))
 
     main()
 
